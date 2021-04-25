101cac9d0b4aa9d2dc7023e0e5ed92cf7cb2ef5b
julia <i.havent.email@google.com>
2021-04-25 22:52:18 +0700

    bot part 3

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # bot
 
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
+# print('Hello! My name is Aid.')
+# print('I was created in 2020.')
+# print('Please, remind me your name.')
 
-your_name = input('>')
-print('What a great name you have,', your_name + '!')
+# your_name = input('пиши тут >')
+# print('What a great name you have,', your_name + '!')
 
+
+# n = int(input())
+# a = n + n
+# print(a)
+# a = a * n
+# print(a)
+# a = a - n
+# print(a)
+# a = a // n
+# print(a)
+days = int(input())
+total_food = int(input())
+one_way_flight = int(input())
+cost_of_one_night = int(input())
+sum_vacation_food = total_food * days
+sum_flight = 2 * one_way_flight
+hotel = (days - 1) * cost_of_one_night
+print(sum_vacation_food + sum_flight + hotel)
 
