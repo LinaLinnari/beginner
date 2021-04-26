3376a968729bf7a1817ca5dfd353c9d44e53dc23
julia <i.havent.email@google.com>
2021-04-26 22:42:58 +0700

    bot part 4

--- main.py
+++ main.py
@@ -17,12 +17,41 @@
 # print(a)
 # a = a // n
 # print(a)
-days = int(input())
-total_food = int(input())
-one_way_flight = int(input())
-cost_of_one_night = int(input())
-sum_vacation_food = total_food * days
-sum_flight = 2 * one_way_flight
-hotel = (days - 1) * cost_of_one_night
-print(sum_vacation_food + sum_flight + hotel)
+# days = int(input())
+# total_food = int(input())
+# one_way_flight = int(input())
+# cost_of_one_night = int(input())
+# sum_vacation_food = total_food * days
+# sum_flight = 2 * one_way_flight
+# hotel = (days - 1) * cost_of_one_night
+# print(sum_vacation_food + sum_flight + hotel)
 
+# first, second, third = int(input()), int(input()), int(input())
+# print((first // 2 + first % 2) + (second // 2 + second % 2) + (third // 2 + third % 2))
+# three_digit_integer = int(input())
+# a = three_digit_integer // 100
+# b = three_digit_integer // 10 % 10
+# c = three_digit_integer % 10
+# print(a + b + c)
+# print('Hello! My name is Aid.')
+# print('I was created in 2020.')
+# print('Please, remind me your name.')
+#
+# name = input('>')
+#
+# print('What a great name you have, ' + name + '!')
+# print('Let me guess your age.')
+# print('Enter remainders of dividing your age by 3, 5 and 7.')
+# remainder3 = int(input('>'))
+# remainder5 = int(input('>'))
+# remainder7 = int(input('>'))
+# age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+#
+# print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+# print(2 ** 3 % 20)
+
+truthy_integer = False or False and True
+print(truthy_integer)
+tricky = not 10
+print(tricky)
