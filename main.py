1e007a180ecc9098fd2246fe3ce4248f7673ae56
rke <imail.for.iwork@gmail.com>
2021-05-09 17:13:20 +0700

    done

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 
-score = int(input())
-maximum = int(input())
-percentages_are_as_follows = (score * 100) / maximum
-if percentages_are_as_follows < 60:
-    print("F")
-elif 60 <= percentages_are_as_follows < 70:
-    print('D')
-elif 70 <= percentages_are_as_follows < 80:
-    print('C')
-elif 80 <= percentages_are_as_follows < 90:
-    print('B')
-elif 90 <= percentages_are_as_follows < 100:
-    print('A')
+def check_cords(x, y):
+    moves = 8
+    edgedX = False
+    if x == 1 or x == 8:
+        edgedX = True
+        moves -= 3
+    if y == 1 or y == 8:
+        if edgedX:
+            moves -= 2
+        else:
+            moves -= 3
+    return moves
+
+
+cordX = int(input('insert X >'))
+cordY = int(input('insert Y >'))
+
+
+print('King may move to ', check_cords(cordX, cordY), ' ways.')
+
