94a69d83cd953fd4c2426bf63e00f17ce46db407
julia <i.havent.email@google.com>
2021-05-11 14:25:34 +0700

    cords fixed checked

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def cords():
     if x == 0 and y == 0:
         print("It's the origin!")
-    if (x == 0 or y == 0) and x != y:
+    elif (x == 0 or y == 0) and x != y:
         print("One of the coordinates is equal to zero!")
-    if x >= 0 and y >= 0:
-        print('I')
-    if x <= 0 and y <= 0:
-        print('III')
-    if x >= 0 and 0 >= y:
-        print('IV')
-    if x <= 0 and 0 <= y:
-        print('II')
+    else:
+        if x >= 0 and y >= 0:
+            print('I')
+        if x <= 0 and y <= 0:
+            print('III')
+        if x >= 0 and 0 >= y:
+            print('IV')
+        if x <= 0 and 0 <= y:
+            print('II')
 
 
 x = float(input())
