925634e20c48774e733c6e60fe739bf2e156e7cd
julia <i.havent.email@google.com>
2021-05-11 14:14:27 +0700

    cords done

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-
-def check_cords(x, y):
-    moves = 8
-    edged_x = False
-    if x == 1 or x == 8:
-        edged_x = True
-        moves -= 3
-    if y == 1 or y == 8:
-        if edged_x:
-            moves -= 2
-        else:
-            moves -= 3
-    return moves
-
-
-cordX = int(input('insert X (1-8) >'))
-cordY = int(input('insert Y (1-8) >'))
+def cords():
+    if x == 0 and y == 0:
+        print("It's the origin!")
+    if (x == 0 or y == 0) and x != y:
+        print("One of the coordinates is equal to zero!")
+    if x >= 0 and y >= 0:
+        print('I')
+    if x <= 0 and y <= 0:
+        print('III')
+    if x >= 0 and 0 >= y:
+        print('IV')
+    if x <= 0 and 0 <= y:
+        print('II')
 
 
-print('King may move to ', check_cords(cordX, cordY), ' ways.')
+x = float(input())
+y = float(input())
+cords()
 
