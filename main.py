a5d8f6fdafbc607a4df72c15a6241a9d89e60259
rke <imail.for.iwork@gmail.com>
2021-05-16 22:12:09 +0700

    some solved

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+data = [1, 2, 3, 4, 1]
 
-score = int(input())
-maximum = int(input())
-percentages_are_as_follows = (score * 100) / maximum
-if percentages_are_as_follows < 60:
-    print("F")
-elif 60 <= percentages_are_as_follows < 70:
-    print('D')
-elif 70 <= percentages_are_as_follows < 80:
-    print('C')
-elif 80 <= percentages_are_as_follows < 90:
-    print('B')
-elif 90 <= percentages_are_as_follows < 100:
-    print('A')
+out = [data[i] if i == 0 else data[i - 1] + data[i] for i, _ in enumerate(data)]
+
+print (out)
