0d9576571c6d5216a57c702b31d58b50160459b1
rke <imail.for.iwork@gmail.com>
2021-05-16 23:22:31 +0700

    solve #2

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 data = [int(n) for n in input()]
 
 
-def compute(i, arr):
-    if i == 0:
-        return arr[0]
-    result = 0
-    for j in range(0, i+1):
-        result += arr[j]
-    return result
-
-
-out = [compute(i, data) for i, _ in enumerate(data)]
+out = [sum(data[0:i+1]) for i, _ in enumerate(data)]
 
 print(out)
