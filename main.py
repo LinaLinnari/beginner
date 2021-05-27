08379f7bb3dcbc43641afbe1c96f7e6a6a3a69e1
rke <imail.for.iwork@gmail.com>
2021-05-27 18:02:16 +0700

    done

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
+a = input().split('-')
 
-def check_cords(x, y):
-    moves = 8
-    edged_x = False
-    if x == 1 or x == 8:
-        edged_x = True
-        moves -= 3
-    if y == 1 or y == 8:
-        if edged_x:
-            moves -= 2
-        else:
-            moves -= 3
-    return moves
-
-
-cordX = int(input('insert X (1-8) >'))
-cordY = int(input('insert Y (1-8) >'))
-
-
-print('King may move to ', check_cords(cordX, cordY), ' ways.')
+for val in a:
+    print(val)
 
